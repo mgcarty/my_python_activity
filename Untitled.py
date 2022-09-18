7688fe2b5758aea47e7914bc4775ac311965bce3
mgcarty <108930554+mgcarty@users.noreply.github.com>
2022-09-18 14:09:21 +0100

    1.2.5 practice

---
+++ Untitled.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+print("My name is Michael")
+print("The date is 18 September 2022")
+
+
+# In[6]:
+
+
+# Write a program to analyse real estate data
+
+#Total montly sales
+a,b,c,d,e = (45000,23400,67000,34600,12900)
+print(a+b+c+d+e)
+
+
+# In[7]:
+
+
+# Average sales per month
+a,b,c,d,e = (45000,23400,67000,34600,12900)
+print((a+b+c+d+e)/5)
+
+
+# In[8]:
+
+
+# Property price for Property C
+
+c - 6500
+
+
+# In[ ]:
+
+
+
+
